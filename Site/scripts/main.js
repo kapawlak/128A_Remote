44e39b6429305d247ff0796fe5a049e9b224ff51
Kelly <kap09c@gmail.com>
2020-12-04 06:00:31 -0800

    insomnia update 2: now all on same page, yay so fancy

--- Site/scripts/main.js
+++ Site/scripts/main.js
@@ -68,26 +68,54 @@ function includeHTML() {
             if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
             /*remove the attribute, and call this function once more:*/
             elmnt.removeAttribute("w3-include-html");
-            includeHTML();
+            updateRoutine()
+            includeHTML()
           }
         }      
         xhttp.open("GET", file, true);
         xhttp.send();
         /*exit the function:*/
-        movefig()
-        renderMathInElement(document.body, {
-          delimiters:[
-            {left: "$$", right: "$$", display: true},
-            {left: "$", right: "$", display: false},
-            {left: "\\(", right: "\\)", display: false},
-            {left: "\\[", right: "\\]", display: true}
-          ]})
-        idheaders()
+        updateRoutine();
         return;
         
       }
     }
+  };
+  
+  function activeincludeHTML(file) {
+    var z, xhttp;
+    /*loop through a collection of all HTML elements:*/
+    z = document.getElementById("mdcontent");
+    
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {z.innerHTML = doRendering(this.responseText);}
+          if (this.status == 404) {z.innerHTML = "<h1>This lab is not available yet!</h1>";}
+          updateRoutine();
+          /*remove the attribute, and call this function once more:*/
+        }
+      }      
+        xhttp.open("GET", file, true);
+        xhttp.send();
+        /*exit the function:*/
+        return;
+        
+      }
+    
+    
+  };
+
+  function changepage(file){
+
+
+    activeincludeHTML(file)
+  }
+  function updateRoutine(){
     idheaders()
+    movefig()
     renderMathInElement(document.body, {
       delimiters:[
         {left: "$$", right: "$$", display: true},
@@ -96,8 +124,7 @@ function includeHTML() {
         {left: "\\[", right: "\\]", display: true}
       ]})
 
-  };
-
+  }
 
 
 
